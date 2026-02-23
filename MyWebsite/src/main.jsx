9558860479a4a4d4e5ebe78faac48cea712989f9
Charl Christopher M. Peñada <cmpenada@student.apc.edu.ph>
2026-02-23 12:10:21 +0800

    Fixed the error in the file naming in main.jsx

--- MyWebsite/src/main.jsx
+++ MyWebsite/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '.css/components.css'
-import '.css/main.css'
-import '.css/sections.css'
+import './css/components.css'
+import './css/main.css'
+import './css/sections.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
